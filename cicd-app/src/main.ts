5c59f8e18d1e9180937c59498ebf7f1ffa7d86fd
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-04-30 16:03:37 +0200

    add city module

--- cicd-app/src/main.ts
+++ cicd-app/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port: number = configService.get('PORT') || 3000;
+
+  console.log("Listening on http://localhost:" + port + " ...");
+
+  await app.listen(port);
 }
 bootstrap();
